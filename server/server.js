be6b38bfafb88ba0797d72529818e9d9e3ae28e8
Prasanna <prasannagithub1@gmail.com>
2026-05-29 15:25:10 +0530

    Integrate Socket.IO server for realtime communication

--- server/server.js
+++ server/server.js
@@ -1,17 +1,40 @@
 import express from "express";
 import cors from "cors";
+import http from "http";
+
+import { Server } from "socket.io";
+
 import documentRoutes from "./routes/documentRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+app.set("io", io);
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
 app.use("/api/documents", documentRoutes);
 
 app.get("/", (req, res) => {
-    res.send("API running...");
+  res.send("API running...");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
